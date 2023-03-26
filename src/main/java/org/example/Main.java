f0e75b1a76698c01bff056d57eadf413cada9dab
iagoholekdev <augustinhocarrara43@gmail.com>
2023-03-26 02:38:29 -0300

    Teste instalação Linux

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String nome = "";
+        int age = 18;
+        if (age == 18) {
+            nome = "Iago";
+        }
+        System.out.println(nome);
+    }
+}
